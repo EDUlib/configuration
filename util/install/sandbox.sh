dc200d0356bce87ebd2a97a360b9b05f976e8904
Pierre Mailhot <pierre.mailhot@gmail.com>
2014-11-27 12:50:21 -0500

    Adding more debugging

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -43,6 +43,7 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 ## Did we specify an openedx release?
+echo $OPENEDX_RELEASE
 if [ -n "$OPENEDX_RELEASE" ]; then
   EXTRA_VARS="-e edx_platform_version=$OPENEDX_RELEASE \
     -e certs_version=$OPENEDX_RELEASE \
