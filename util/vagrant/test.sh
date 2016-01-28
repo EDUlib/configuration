be8ffbcf396e96ba64ee305b48bf2a4715f1921f
Ubuntu <ubuntu@ip-10-0-0-71.ec2.internal>
2016-01-28 13:45:15 -0500

    Test pour régler le problème de .rbenv et .gem pour les forums

---
+++ util/vagrant/test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+OPENEDX_ROOT="/edx"
+
+echo "Avant installation de la derniere partie"
+
+if [[ -f /edx/app/edx_ansible/server-vars.yml ]]; then
+  SERVER_VARS="--extra-vars=\"@${OPENEDX_ROOT}/app/edx_ansible/server-vars.yml\""
+fi
+
+cd /var/tmp/configuration/playbooks
+echo "edx_platform_repo: 'https://github.com/EDUlib/edx-platform.git'" > vars.yml
+echo "edx_platform_version: $TARGET" >> vars.yml
+echo "ora2_source_repo: 'https://github.com/EDUlib/edx-ora2.git'" >> vars.yml
+echo "ora2_version: $TARGET" >> vars.yml
+echo "CERTS_REPO: 'https://github.com/EDUlib/edx-certificates.git'" >> vars.yml
+echo "certs_version: $TARGET" >> vars.yml
+echo "forum_source_repo: 'https://github.com/EDUlib/cs_comments_service.git'" >> vars.yml
+echo "forum_version: $TARGET" >> vars.yml
+echo "xqueue_source_repo: 'https://github.com/EDUlib/xqueue.git'" >> vars.yml
+echo "xqueue_version: $TARGET" >> vars.yml
+sudo ansible-playbook \
+    --inventory-file=localhost, \
+    --connection=local \
+    --extra-vars="@vars.yml" \
+    $SERVER_VARS \
+    vagrant-fullstack.yml
+
+echo "On devrait pouvoir rebooter"
