0c0a98f08ee9818665f0b4f3ae95fb1a53ac9db5
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-02-11 13:21:18 -0500

    from edulib-dogwood-rc to edulib-dogwood

--- util/vagrant/upgrade.sh
+++ util/vagrant/upgrade.sh
@@ -297,7 +297,7 @@ fi
 # Update to target.
 
 echo "Updating to final version of code"
-echo "After Target Dogwood before edulib-dogwood-rc"
+echo "After Target Dogwood before edulib-dogwood"
 
 cd configuration/playbooks
 echo "edx_platform_repo: 'https://github.com/EDUlib/edx-platform.git'" > vars.yml
