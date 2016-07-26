6531111ddaaf7d5770c20120ac7ce1a1eae4a16f
Ubuntu <ubuntu@ip-10-0-0-71.ec2.internal>
2016-07-26 11:22:18 -0400

    extra-vars instead of extra_vars

--- util/vagrant/upgrade.sh
+++ util/vagrant/upgrade.sh
@@ -323,8 +323,8 @@ if [[ $TARGET == *eucalyptus* ]] ; then
     --extra-vars="ora2_version=$TARGET" \
     --extra-vars="CERTS_REPO=https://github.com/EDUlib/edx-certificates.git" \
     --extra-vars="certs_version=$TARGET" \
-    --extra_vars="NOTIFIER_SOURCE_REPO=https://github.com/EDUlib/notifier.git" \
-    --extra_vars="NOTIFIER_VERSION=$TARGET" \
+    --extra-vars="NOTIFIER_SOURCE_REPO=https://github.com/EDUlib/notifier.git" \
+    --extra-vars="NOTIFIER_VERSION=$TARGET" \
     --extra-vars="migrate_db=no" \
     --skip-tags="edxapp-sandbox,gather_static_assets" \
     vagrant-$CONFIGURATION-delta.yml
