719f0160436711cd2419385f5d4d7de430075c8c
Pierre Mailhot <pierremailhot@Pierres-iMac.local>
2017-02-08 20:43:01 -0500

    EDULIB not installing ecommerce ecomworker programs insights locally

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -46,6 +46,7 @@ OPENEDX_RELEASE="edulib-ficus.1rc3"
 ## Overridable version variables in the playbooks. Each can be overridden
 ## individually, or with $OPENEDX_RELEASE.
 ##
+### EDULIB NOT INSTALLING PROGRAMS_VERSION
 VERSION_VARS=(
   edx_platform_version
   certs_version
@@ -97,7 +98,6 @@ if [ -n "$OPENEDX_RELEASE" ]; then
     -e NOTIFIER_VERSION=$OPENEDX_RELEASE \
     -e INSIGHTS_VERSION=$OPENEDX_RELEASE \
     -e ANALYTICS_API_VERSION=$OPENEDX_RELEASE \
-    -e PROGRAMS_VERSION=$OPENEDX_RELEASE \
   $EXTRA_VARS"
 fi
 
