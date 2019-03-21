9ff8e8fc26e14e274767c22efa94af69a3ff0371
sambapete <pierre.mailhot@gmail.com>
2019-03-21 14:25:00 -0400

    from edulib-ironwood.1rc4 to edulib-ironwood.1 in configuration files

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -27,7 +27,7 @@ fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
 #####    CONFIGURATION_VERSION=${OPENEDX_RELEASE-master}
-  CONFIGURATION_VERSION="edulib-ironwood.1rc4"
+  CONFIGURATION_VERSION="edulib-ironwood.1"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
