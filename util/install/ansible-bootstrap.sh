8793b17f84f69f8ac462231416bd0f58eb450cf7
Pierre Mailhot <pierremailhot@Pierres-Mac-mini.local>
2020-05-28 01:05:38 -0400

    from edulib-juniper.rc3 to edulib-juniper.master 5 files modified

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -27,7 +27,7 @@ fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
 #####    CONFIGURATION_VERSION=${OPENEDX_RELEASE-master}
-  CONFIGURATION_VERSION="edulib-juniper.rc3"
+  CONFIGURATION_VERSION="edulib-juniper.master"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
