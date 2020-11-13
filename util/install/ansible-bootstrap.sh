33eb7f940731012548a9ab49e2dbb8d0349dc0ce
Pierre Mailhot <pierremailhot@Pierres-Mac-mini.local>
2020-11-13 09:23:38 -0500

    change release from edulib-juniper.3 to edulib-koa.test01

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -27,7 +27,7 @@ fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
 #####    CONFIGURATION_VERSION=${OPENEDX_RELEASE-master}
-  CONFIGURATION_VERSION="edulib-juniper.3"
+  CONFIGURATION_VERSION="edulib-koa.test01"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
