9816670a8013afa2e926dfa7fdfe4a92eaaefc09
Pierre Mailhot <pierremailhot@Pierres-Mac-mini.local>
2020-11-13 09:23:38 -0500

    change release from edulib-juniper.3 to edulib-koa.test01

--- util/install/native.sh
+++ util/install/native.sh
@@ -109,8 +109,8 @@ sudo -H pip install --upgrade virtualenv==16.7.10
 ##
 ## MODIFS PAR EDULIB 20200522
 ##
-CONFIGURATION_VERSION="edulib-juniper.3"
-OPENEDX_RELEASE="edulib-juniper.3"
+CONFIGURATION_VERSION="edulib-koa.test01"
+OPENEDX_RELEASE="edulib-koa.test01"
 
 ##
 ## Overridable version variables in the playbooks. Each can be overridden
