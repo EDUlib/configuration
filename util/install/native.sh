ee43d8cc3a8c63ec00276a8dc530e36a075c1c19
Pierre Mailhot <pierremailhot@Pierres-Mac-mini.local>
2020-12-02 01:10:15 -0500

    from edulib-koa.test01 to edulib-koa.test02

--- util/install/native.sh
+++ util/install/native.sh
@@ -109,8 +109,8 @@ sudo -H pip install --upgrade virtualenv==16.7.10
 ##
 ## MODIFS PAR EDULIB 20200522
 ##
-CONFIGURATION_VERSION="edulib-koa.test01"
-OPENEDX_RELEASE="edulib-koa.test01"
+CONFIGURATION_VERSION="edulib-koa.test02"
+OPENEDX_RELEASE="edulib-koa.test02"
 
 ##
 ## Overridable version variables in the playbooks. Each can be overridden
